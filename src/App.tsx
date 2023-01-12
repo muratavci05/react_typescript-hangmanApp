b1445f4da482142d5ddc2bba72fe13a6879bb4a9
muratavci05 <js.muratavci@gmail.com>
2023-01-13 00:51:39 +0300

    wordList file add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
+import words from "./wordList.json";
 
 function App() {
- 
-
+  const [wordToGuess, setWordToGuess] = useState(()=> {
+    return words[Math.floor(Math.random() * words.length)]
+  });
   return (
     <div className="App">
-    <p>hello</p>
+      <p>hello</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
