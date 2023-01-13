f81ab0375e40e70bbc7caacc92923d9a0bfeee82
muratavci05 <js.muratavci@gmail.com>
2023-01-13 15:13:42 +0300

    component in hangmanword

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,3 +1,25 @@
-export function HangmanWord () {
-    
+export function HangmanWord() {
+const guessedLetters = ["t","e","g","t"]
+  const word = "test";
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "0.25em",
+        fontSize: "6rem",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: "0.1em solid black" }} key={index}>
+          <span style={{ visibility: guessedLetters.includes(letter)
+          ? "visible"
+          : "hidden",
+        
+        }}>{letter}</span>
+        </span>
+      ))}
+    </div>
+  );
 }
